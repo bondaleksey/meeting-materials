730f19942b19834ffa0a7da79b5a136aba6f1881
Aleksey Bondarenko <bondaleksey@gmail.com>
2025-04-09 23:50:48 +0300

    add respond server

---
+++ topics/asyncio/ch-03/telnet-01/server-03-respond.py
@@ -0,0 +1,28 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(("127.0.0.1", 8008))
+server_socket.listen()
+
+try:
+    connection, client_address = server_socket.accept()
+    print(f"Connection request received from {client_address}")
+
+    buffer = b""
+
+    while buffer[-2:] != b"\r\n":
+        data = connection.recv(2)
+        if not data:
+            break
+        else:
+            print(f"Received data: {data}!")
+            buffer = buffer + data
+
+    print(f"All data received: {buffer}")
+    connection.sendall(b"HTTP/1.0 200 OK\r\n\r\n")
+    connection.sendall(buffer)
+finally:
+    print("Closing server socket...")
+    server_socket.close()
+    print("Server socket closed.")
